49ccf84478732ac4fe9be7f6ef32530cb6e87f1d
raonever <ever29lkh@gmail.com>
2020-09-16 02:59:35 +0900

    sub 171 adding interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // Edit request config
+    return request;     // return을 하지 않으면 요청이 차단됨
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // Edit request config
+    return response;     // return을 하지 않으면 요청이 차단됨
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
