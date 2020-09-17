789b6d1f328b644f621d4d98ee84f05dbf38e22b
raonever <ever29lkh@gmail.com>
2020-09-17 16:39:51 +0900

    sub 195~197 route path exact, link, props

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render () {
@@ -12,14 +13,19 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />   
                 <Route path="/" render={() => <h1>Home 2</h1>} />    */}
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post"  component={NewPost} />
             </div>
         );
     }
@@ -28,3 +34,4 @@ class Blog extends Component {
 export default Blog;
 
 // exact : 정확한 path 에서만 작동, exact가 없으면 다른 path를 이동해도 기존 path 유지
+// exact를 제거하는 경우 : 해당 path 뒤에 다른 path를 이용하기 위해서 제거
